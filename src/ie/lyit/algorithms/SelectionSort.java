18452bfdc054d78ac6cbcec6190a0c6ebffba25b
Fallon77 <fallonjames32@gmail.com>
2018-04-21 19:44:46 +0100

    Merge origin/master
    Conflicts:
    	src/ie/lyit/algorithms/BubbleSort.java
    	src/ie/lyit/algorithms/EnhancedBubble.java
    	src/ie/lyit/arrays/InvertedArray.java
    	src/ie/lyit/arrays/SortedArray.java
    	src/ie/lyit/arrays/UnSortedArray.java

---
+++ src/ie/lyit/algorithms/SelectionSort.java
@@ -0,0 +1,48 @@
+package ie.lyit.algorithms;
+
+/**
+ * Author James Fallon
+ * -- Selection sort --
+ */
+
+public class SelectionSort {
+    
+    private long startTime;
+    private long finishTime;
+    private long swaps;
+    private int comparsions;
+    
+    public void selectionSort(int[] array){	
+	startTime = System.nanoTime();
+        swaps =0;
+        comparsions =0;
+	for(int i=0;i<array.length-1;i++){
+	int min = i;
+            for(int j=i+1;j<array.length;j++){
+            comparsions++;  
+            if(array[j] < array[min]){
+            min  = j;
+		}
+            }
+            // Swap values
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+            swaps++;
+	}
+	finishTime = (System.nanoTime()- startTime);
+	}
+    // Getter's
+    public long getFinishTime() {
+        return finishTime;
+    }
+
+    public long getSwaps() {
+        return swaps;
+    }
+
+    public int getComparsions() {
+        return comparsions;
+    }
+    
+}
